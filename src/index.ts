3c71f0f66f6071b62adba979380e8ab397924d6e
Nico Salvador <salvador.nico@gmail.com>
2018-12-04 21:42:21 +0800

    Initial test

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+const MagicHomeDiscovery = require("magic-home").Discovery
+const MagicHomeControl = require("magic-home").Control
+
+type Device = { address: string; id: string; model: string }
+
+const discovery = new MagicHomeDiscovery()
+
+discovery
+	.scan(500, function(err: any, devices: Device[]) {
+		console.log(devices)
+	})
+	.then((devices: Device[]) => {
+		devices.forEach(device => {
+			const light = new MagicHomeControl(device.address)
+			light.turnOn((err: any, success: boolean) => {
+				console.log(`Turning on ${device.model}`)
+			})
+
+			setTimeout(() => {
+				light.turnOff(() => {
+					console.log(`Turning off ${device.model}`)
+				})
+			}, 2000)
+		})
+	})
